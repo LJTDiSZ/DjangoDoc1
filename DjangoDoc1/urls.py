65d1a6fba721f2008ded01bbfa140123bbc50c83
LJTDiSZ <jiang.github@163.com>
2015-07-17 17:16:21 +0800

    Initial

---
+++ DjangoDoc1/urls.py
@@ -0,0 +1,21 @@
+﻿"""
+Definition of urls for DjangoDoc1.
+"""
+
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'DjangoDoc1.views.home', name='home'),
+    # url(r'^DjangoDoc1/', include('DjangoDoc1.DjangoDoc1.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
