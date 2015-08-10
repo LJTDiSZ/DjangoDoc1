3ae0497425e315ccd191752653c899e09398c583
LJTDiSZ <jiang.github@163.com>
2015-08-10 14:27:08 +0800

    test & static files

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
@@ -7,20 +7,52 @@ Replace this with more appropriate tests for your application.
 
 import django
 from django.test import TestCase
+from django.utils import timezone
+
+import datetime
+from .models import Question
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
-class SimpleTest(TestCase):
-    """Tests for the application views."""
+#class SimpleTest(TestCase):
+#    """Tests for the application views."""
 
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            django.setup()
+#    if django.VERSION[:2] >= (1, 7):
+#        # Django 1.7 requires an explicit setup() when running tests in PTVS
+#        @classmethod
+#        def setUpClass(cls):
+#            django.setup()
 
-    def test_basic_addition(self):
+#    def test_basic_addition(self):
+#        """
+#        Tests that 1 + 1 always equals 2.
+#        """
+#        self.assertEqual(1 + 1, 2)
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
         """
-        Tests that 1 + 1 always equals 2.
+         was_published_recently() should return False for questions whose pub_date is in the future.
         """
-        self.assertEqual(1 + 1, 2)
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
